45fb439e9601e5d1f4fbbb34d22fb74b4eaa7b13
Merlru <virapat46@gmail.com>
2023-06-02 13:17:53 +0700

    adding routes

--- src/App.js
+++ src/App.js
@@ -3,17 +3,19 @@ import { Routes, Route } from 'react-router-dom';
 // Components Path
 import Home from "./Routes/home/home";
 import Navigation from './Routes/navigation/navigation';
+import SignIn from './Routes/sign-in/sign-in';
 
 const Shop = () => {
   return <h1>I am the shop page</h1>
-}
+};
 
 const App = () => {
   return (
     <Routes>
       <Route path='/' element={<Navigation/>}>
         <Route index element={<Home/>}/>
-        <Route path='shop' element={<Shop/>}/> 
+        <Route path='shop' element={<Shop/>}/>
+        <Route path='sign-in' element={<SignIn/>}/>
       </Route>
     </Routes>
   );
