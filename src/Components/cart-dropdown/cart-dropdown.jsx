78f53bcb4fbc9959dd47a21622266fa404b53584
Merlru <virapat46@gmail.com>
2023-07-13 09:58:54 +0700

    added cart that able to receive add to cart and increase quantity

--- src/Components/cart-dropdown/cart-dropdown.jsx
+++ src/Components/cart-dropdown/cart-dropdown.jsx
@@ -1,10 +1,22 @@
-import './cart-dropdown.scss';
+import { useContext } from 'react';
+
+import { CartContext } from '../../Contexts/cart.context';
+
+import CartItem from '../cart-item/cart-item';
 import Button from '../button/button';
 
+import './cart-dropdown.scss';
+
 const CartDropdown = () => {
+    const { cartItems } = useContext(CartContext);
+
     return (
         <div className='cart-dropdown-container'>
-            <div className='cart-items'/>
+            <div className='cart-items'>
+                {cartItems.map((item) => (
+                    <CartItem key={item.id} cartItem={item}/>
+                ))}
+            </div>
             <Button>GO TO CHECKOUT</Button>
         </div>
     );
---
+++ src/Components/cart-item/cart-item.jsx
@@ -0,0 +1,15 @@
+import './cart-item.scss';
+
+const CartItem = ({cartItem}) => {
+    const { name, quantity } = cartItem;
+    // console.log(cartItem)
+
+    return (
+        <div>
+            <h2>{name}</h2>
+            <span>{quantity}</span>
+        </div>
+    );
+};
+
+export default CartItem;
